635ce7e6a6196008e30e5eba5e3a9b2ec73ebbf1
flowsar <brahim.sar151@gmail.com>
2025-08-27 23:20:55 +0100

    challenges exercise

--- week_2/day_1/daily_challenge/challenges.sql
+++ week_2/day_1/daily_challenge/challenges.sql
@@ -1,5 +1,7 @@
-// ====== Daily Challenge 1
+-- ====== Daily Challenge 1
 
-// ====== Daily Challenge 2
+select count(*) from actors;
 
-// ====== Daily Challenge 3
+
+insert into actors (first_name) values ('brahim');
+-- If we add a new actor with some blank fields, we will get an error because the other fields can't accept null as a value.
