6c1c051d182f411b390e1da097642a1524bcd511
flowsar <brahim.sar151@gmail.com>
2025-08-27 23:22:07 +0100

    update to upper-case

--- week_2/day_1/daily_challenge/challenges.sql
+++ week_2/day_1/daily_challenge/challenges.sql
@@ -1,7 +1,7 @@
 -- ====== Daily Challenge 1
 
-select count(*) from actors;
+SELECT count(*) FROM actors;
 
 
-insert into actors (first_name) values ('brahim');
+INSERT INTO actors (first_name) VALUES ('brahim');
 -- If we add a new actor with some blank fields, we will get an error because the other fields can't accept null as a value.
