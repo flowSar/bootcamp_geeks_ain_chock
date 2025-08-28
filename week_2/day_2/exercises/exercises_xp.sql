5b0ef0f0faf387de6b9eff11f9b46560aa86179f
flowsar <brahim.sar151@gmail.com>
2025-08-28 23:59:06 +0100

    sql exercise

--- week_2/day_2/exercises/exercises_xp.sql
+++ week_2/day_2/exercises/exercises_xp.sql
@@ -1,9 +1,51 @@
-// ===== Exercise 1
+-- ===== Exercise 1
 
-// ===== Exercise 2
+SELECT * FROM items order by price ASC;
 
-// ===== Exercise 3
+SELECT * FROM items WHERE price >= 80 ORDER BY price DESC;
 
-// ===== Exercise 4
+SELECT first_name, last_name FROM customers ORDER BY first_name ASC LIMIT 3;
 
-// ===== Exercise ...
+SELECT last_name FROM customers ORDER BY last_name DESC;
+
+
+-- ===== Exercise 2
+
+-- question 01
+SELECT * FROM customer;
+
+-- question 02
+SELECT first_name, last_name AS full_name FROM names;
+
+-- question 03
+SELECT distinct create_date FROM customer;
+
+-- question 04
+SELECT * FROM customer ORDER BY first_name DESC;
+
+-- question 05
+SELECT film_id, title, description, release_year, rental_rate FROM film ORDER BY rental_rate ASC;
+
+-- question 06
+SELECT address, phone FROM customer JOIN address ON customer.address_id=address.address_id WHERE address.district='Texas';
+
+-- question 07
+SELECT * FROM film WHERE film_id IN(15, 150);
+
+-- question 08
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title='Airport Pollock';
+
+-- question 09
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title ~* '^Ai';
+
+-- question 10
+SELECT * FROM film ORDER BY replacement_cost ASC LIMIT 10;
+
+-- question 11
+
+-- question 12
+
+-- question 13
+
+-- question 14
+
