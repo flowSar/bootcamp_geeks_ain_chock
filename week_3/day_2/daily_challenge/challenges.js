cc39c25e4aeda4ad694e589874431e9c6389aabb
flowsar <brahim.sar151@gmail.com>
2025-09-10 10:06:36 +0100

    daily chllange

--- week_3/day_2/daily_challenge/challenges.js
+++ week_3/day_2/daily_challenge/challenges.js
@@ -1,5 +1,23 @@
 // ====== Daily Challenge 1
 
-// ====== Daily Challenge 2
+const sentence = "This movie is not so bad !";
 
-// ====== Daily Challenge 3
+let wordNot = sentence.indexOf("not");
+
+let wordBad = sentence.indexOf("bad");
+
+let newSentence = "";
+
+if (wordBad !== -1 && wordBad != -1) {
+  let i = 0;
+  for (i; i < sentence.length; i++) {
+    if (i == wordNot) {
+      newSentence += "good";
+      i = wordBad + 3;
+    }
+    newSentence += sentence[i];
+  }
+  console.log(newSentence);
+} else {
+  console.log(sentence);
+}
