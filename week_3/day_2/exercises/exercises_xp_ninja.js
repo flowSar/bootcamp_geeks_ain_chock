d11c5f77e874a67f6547102e32efc88ef4089706
flowsar <brahim.sar151@gmail.com>
2025-09-10 11:49:54 +0100

    Exercises XP NinjaExercises XP Ninja

--- week_3/day_2/exercises/exercises_xp_ninja.js
+++ week_3/day_2/exercises/exercises_xp_ninja.js
@@ -1,9 +1,53 @@
 // ===== Exercise 1
+let personOne = {
+  FullName: "Khalid",
+  Mass: 75,
+  Height: 1.8,
+  bmi: function () {
+    return this.Mass / this.Height ** 2;
+  },
+};
+
+let personTwo = {
+  FullName: "Yassine",
+  Mass: 68,
+  Height: 1.75,
+  bmi: function () {
+    return this.Mass / this.Height;
+  },
+};
+
+console.log(personOne.bmi());
+
+function compareBmi() {
+  if (personOne.bmi() > personTwo.bmi()) {
+    console.log(personOne.FullName);
+  }
+  console.log(personTwo.FullName);
+}
+compareBmi();
 
 // ===== Exercise 2
 
-// ===== Exercise 3
+const findAvg = (gradesList) => {
+  // get the grades list length
+  const length = gradesList.length;
+  let gradesTotal = 0;
+  for (const item of gradesList) {
+    gradesTotal += item;
+  }
+  average = gradesTotal / length;
+  return average;
+};
 
-// ===== Exercise 4
+const pritnResut = (gradesList) => {
+  const average = findAvg(gradesList);
+  if (average > 65) {
+    console.log("congratulation you passed");
+  } else if (average < 65) {
+    console.log("unfortunately you failed and must repeat the course");
+  }
+};
 
-// ===== Exercise ...
+pritnResut([60, 50, 100, 100, 70]);
+pritnResut([60, 50, 0, 100, 70]);
